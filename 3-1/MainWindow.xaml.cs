92abd6652436575a83b6e830716fa0e5ea6d4e5f
sketch47 <hamhb777@gmail.com>
2016-12-23 18:37:18 +0300

    ex2

--- 3-1/MainWindow.xaml.cs
+++ 3-1/MainWindow.xaml.cs
@@ -19,15 +19,19 @@ namespace _3_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        double[] mas = new double[10];
+        int i;
+        int j = 0;
         public MainWindow()
         {
+            
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void button1_Click(object sender, RoutedEventArgs e)//ex1
         {
-                int j=0;
-              double[] mas = new double[10];
+                
+             
               mas[0]  = Convert.ToInt32(textBox1.Text);
               mas[1] = Convert.ToInt32(textBox2.Text);
               mas[2]= Convert.ToInt32(textBox3.Text);
@@ -39,10 +43,32 @@ namespace _3_1
               mas[8]= Convert.ToInt32(textBox9.Text);
               mas[9] = Convert.ToInt32(textBox10.Text);
 
-              for (int i = 1; i < 9; i++)
+              for (i = 1; i < 9; i++)
                   if (mas[i] > mas[i - 1] && mas[i] > mas[i + 1]) j++;
 
               out1.Text = "Элементов  больше своих «соседей» =" + Convert.ToString(j);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            mas[0] = Convert.ToInt32(textBox1.Text);
+            mas[1] = Convert.ToInt32(textBox2.Text);
+            mas[2] = Convert.ToInt32(textBox3.Text);
+            mas[3] = Convert.ToInt32(textBox4.Text);
+            mas[4] = Convert.ToInt32(textBox5.Text);
+            mas[5] = Convert.ToInt32(textBox6.Text);
+            mas[6] = Convert.ToInt32(textBox7.Text);
+            mas[7] = Convert.ToInt32(textBox8.Text);
+            mas[8] = Convert.ToInt32(textBox9.Text);
+            mas[9] = Convert.ToInt32(textBox10.Text);
+
+            for (i = 0; i <= 9; i++)
+                if (mas[i] > 25) { j++; break; }
+
+            if(j==0)
+                out1.Text = "Все элименты <25";
+            if(j==1)
+            out1.Text = "Первый элимент >25 =" + (i+1);
         }
     }
 }
