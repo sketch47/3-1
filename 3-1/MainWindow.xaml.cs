19f88272242e3d077bbc96191f71972b94a8da31
sketch47 <hamhb777@gmail.com>
2016-12-24 01:01:23 +0300

    ex9

--- 3-1/MainWindow.xaml.cs
+++ 3-1/MainWindow.xaml.cs
@@ -159,5 +159,26 @@ namespace _3_1
             }
             out1.Text =  Convert.ToString(str_out);
         }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            sum = 0; i = 0;
+            mas[0] = Convert.ToInt32(textBox1.Text);
+            mas[1] = Convert.ToInt32(textBox2.Text);
+            mas[2] = Convert.ToInt32(textBox3.Text);
+            mas[3] = Convert.ToInt32(textBox4.Text);
+            mas[4] = Convert.ToInt32(textBox5.Text);
+            mas[5] = Convert.ToInt32(textBox6.Text);
+            mas[6] = Convert.ToInt32(textBox7.Text);
+            mas[7] = Convert.ToInt32(textBox8.Text);
+            mas[8] = Convert.ToInt32(textBox9.Text);
+            mas[9] = Convert.ToInt32(textBox10.Text);
+
+            for (i = 0; i < 10; i++) {
+                if (mas[i] % 3 != 0)
+                    sum += mas[i];
+            }
+            out1.Text ="Сумма не кратных 3 = "+ Convert.ToString(sum);
+        }
     }
 }
